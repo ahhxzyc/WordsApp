be07266003684cd828b6a9819c81eb55c4746ed1
zyc <1252706876@qq.com>
2020-04-25 20:29:33 +0800

    apply on main list

--- app/src/main/java/com/example/android/wordsapp/MainActivity.java
+++ app/src/main/java/com/example/android/wordsapp/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.loader.app.LoaderManager;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.example.android.wordsapp.data.WordsContract;
 import com.example.android.wordsapp.data.WordsContract.WordsEntry;
 import com.example.android.wordsapp.data.WordsCursorAdapter;
@@ -39,10 +45,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Setup the list view display
-        ListView lvMain = findViewById(R.id.main_list);
+        final SwipeMenuListView lvMain = findViewById(R.id.main_list);
         lvMain.setEmptyView(findViewById(R.id.empty_view));
         mAdapter = new WordsCursorAdapter(this, null); // Initially empty
         lvMain.setAdapter(mAdapter);
+
+        // Create a swipe menu creator
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem itemDel = new SwipeMenuItem(getApplicationContext());
+                itemDel.setWidth(dp2px(140));
+                itemDel.setBackground(R.color.red);
+                itemDel.setIcon(R.drawable.ic_add_pet);
+                menu.addMenuItem(itemDel);
+            }
+            private int dp2px(int dp) {
+                return Math.round(getResources().getDisplayMetrics().density * dp);
+            }
+        };
+        lvMain.setMenuCreator(creator);
+
+        // Set swipe menu click listener
+        lvMain.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                long id = lvMain.getItemIdAtPosition(position);
+                Log.v("OOOOOOOOOOOO", "caught this click: " + id  + " " + index);
+                Uri uri = WordsEntry.buildWordsUri(id);
+                switch (index) {
+                    case 0:
+                        // delete this word
+                        getContentResolver().delete(
+                                uri,
+                                WordsEntry._ID + "=?",
+                                new String[] {String.valueOf(position)}
+                        );
+                        break;
+                }
+                return false;
+            }
+        });
 
         // Setup the Loader on its mission
         WordsLoaderHelper loaderHelper = new WordsListLoaderHelper(
